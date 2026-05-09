fe8f40519dc7998a2cd8961dba2f3258ebebd4e9
freedom <2587765081@qq.com>
2026-05-09 17:11:50 +0800

    登录界面的构建和表单校验

--- lib/utils/toastutils.dart
+++ lib/utils/toastutils.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 
 class ToastUtils {
+  static bool showloading = false;
   static void showToast(BuildContext context, String? msg) {
+    if (showloading) {
+      return;
+    }
+    showloading = true;
+    Future.delayed(Duration(seconds: 2), () {
+      showloading = false;
+    });
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        width: 120,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(40),
-        ),
+        width: 180,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
         behavior: SnackBarBehavior.floating,
-        duration: Duration(seconds: 1),
-        content: Text(msg ?? "加载成功",textAlign: TextAlign.center,)),
+        duration: Duration(seconds: 2),
+        content: Text(msg ?? "加载成功", textAlign: TextAlign.center),
+      ),
     );
   }
 }
